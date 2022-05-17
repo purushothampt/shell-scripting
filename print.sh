bc32fb22585c9ab086e8fa9d01804842a14964c9
Purushotham Pt <purushothampt@gmail.com>
2022-05-16 21:26:09 -0500

    Print escape sequence

--- print.sh
+++ print.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
 echo "Hello World"
+
+# color codes
+# red     31
+# Green   32
+# Yellow  33
+# Blue    34
+# Magenta 35
+# Cyan    36
+
+# syntax : echo -e "\e[31mHello\e[0m"
+
+echo -e "\e[31mHello\e[0m" World
+
