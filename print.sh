104b2ff955ab6da8114d839152fc37adbcafb6bd
Purushotham Pt <purushothampt@gmail.com>
2022-05-16 21:30:17 -0500

    Print escape sequence

--- print.sh
+++ print.sh
@@ -15,4 +15,4 @@ echo "Hello World"
 echo -e "\e[31mHello\e[0m" World
 echo -e "\e[32mHello\e[0m" "\e[31mWorld\e[0m"
 echo -e "\e[33mHello\e[0m" "\e[32mWorld\e[0m"
-echo -e "\n Hello \n World"
+echo -e "Hello \n World"
