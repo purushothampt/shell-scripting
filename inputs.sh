a5eb51c8e24628a60a06cf7228771433b574ef2c
Purushotham Pt <purushothampt@gmail.com>
2022-05-18 13:42:54 -0500

    Input Variables

--- inputs.sh
+++ inputs.sh
@@ -8,3 +8,12 @@ echo First argument = $1
 echo second argument = $2
 echo all arguments = $*
 echo count of arguments = $#
+
+echo 'I have $100 Apples'
+echo "I have $100 Apples"
+echo "I have \$100 Apples"
+
+Training = DevOps
+echo 'My Training is $Training'
+echo "My Training is $Training"
+echo "My Training is \$Training"
