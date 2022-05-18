8a2f4233ad6cf93b26c541dc0dd490861400de0e
Purushotham Pt <purushothampt@gmail.com>
2022-05-18 10:58:49 -0500

    Variables Test 6

--- variables.sh
+++ variables.sh
@@ -27,3 +27,12 @@ add=$(($a + $b))
 mult=$(($a * $b))
 echo the sum is $add
 echo the mult is $mult
+
+#scalar/arrays
+a=(10 20, second "small number")
+echo First value of array is $a[0]
+echo Second value of array is $a[1]
+echo Third value of array is $a[2]
+echo Fourth value of array is $a[3]
+echo All value of array is $a[*]
+echo Count of Array is $#a[*]
