bd098bdcd3aa6f69843eb784ddaa231feb5bf7e0
Purushotham Pt <purushothampt@gmail.com>
2022-05-18 10:21:16 -0500

    Variables Test 6

--- variables.sh
+++ variables.sh
@@ -13,11 +13,17 @@ echo Todays date is $Date
 #command
 Date=$(date +%F)
 Time=$(date +%T)
+Day=$(date +%d)
+month=$(date +%m)
 echo Todays date is $Date
 echo Now the time is $Time
+echo The Day is $Day
+echo The month is $Month
 
 #expression
 a=100
 b=200
 add=$(($a + $b))
+mult=$(($a * $b))
 echo the sum is $add
+echo the mult is $mult
