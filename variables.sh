362428bb8f7f065080db20e00ba08da47e575823
Purushotham Pt <purushothampt@gmail.com>
2022-05-18 11:06:48 -0500

    Variables Test 6

--- variables.sh
+++ variables.sh
@@ -30,9 +30,9 @@ echo the mult is $mult
 
 #scalar/arrays
 a=(10 20, second "small number")
-echo First value of array is $a[0]
-echo Second value of array is $a[1]
-echo Third value of array is $a[2]
-echo Fourth value of array is $a[3]
-echo All value of array is $a[*]
-echo Count of Array is $#a[*]
+echo First value of array is ${a[0]}
+echo Second value of array is ${a[1]}
+echo Third value of array is ${a[2]}
+echo Fourth value of array is ${a[3]}
+echo All value of array is $a{[*]}
+echo Count of Array is ${#a[*]}
