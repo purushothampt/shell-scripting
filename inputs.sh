288aa09e750fb7bfbf23a7a74bf4398867688655
Purushotham Pt <purushothampt@gmail.com>
2022-05-18 12:34:28 -0500

    Input Variables

--- inputs.sh
+++ inputs.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-read -p 'enter your name: ' name
-echo The user entered value is $name
+#read -p 'enter your name: ' name
+#echo The user entered value is $name
+
+echo scipt name = $0
+echo First argument = $1
+echo second argument = $2
