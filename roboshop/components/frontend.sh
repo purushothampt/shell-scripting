88b066068c4e68c9ff28ef45ea2673227f1b0037
Purushotham Pt <purushothampt@gmail.com>
2022-05-23 15:41:09 -0500

    Make file changes 3

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,8 +8,8 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/fron
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
-mv frontend-main/*
-mv static/*
+mv frontend-main/* .
+mv static/* .
 #rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
