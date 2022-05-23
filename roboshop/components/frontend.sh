adee0dc36d4cdc153c76ee851c2c422e44728876
Purushotham Pt <purushothampt@gmail.com>
2022-05-23 15:51:00 -0500

    Make file changes - Echo

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
 systemctl start nginx
 
+echo -e "\e[36m Downloading Nginx Content \e[0m "
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+echo -e "\e[36m Cleanup old content and extract new archive \e[0m"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
@@ -13,5 +16,6 @@ mv static/* .
 #rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[36m Start Nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
