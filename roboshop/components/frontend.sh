dd9d3fcc49039717faacd1b725c3b9240bed3692
Purushotham Pt <purushothampt@gmail.com>
2022-05-24 15:52:14 -0500

    Frontend Changes 6

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,27 +25,27 @@ if [ $USER_ID -ne 0 ]; then
 fi
 
 Print " Installing Nginx "
-yum install nginx ->> $LOG_FILE
+yum install nginx -y &>> $LOG_FILE
 StatCheck $?
 
 Print " Downloading Nginx Content "
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >> LOG_FILE
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> LOG_FILE
 StatCheck $?
 
 Print " Cleanup old content "
-rm -rf /usr/share/nginx/html/*  >> $LOG_FILE
+rm -rf /usr/share/nginx/html/*  &>> $LOG_FILE
 StatCheck $?
 
 cd /usr/share/nginx/html/
 
 Print " Extracting Archive "
-unzip /tmp/frontend.zip >> $LOG_FILE && mv frontend-main/* . >> $LOG_FILE && mv static/* .  >> $LOG_FILE
+unzip /tmp/frontend.zip >> $LOG_FILE && mv frontend-main/* . >> $LOG_FILE && mv static/* .  &>> $LOG_FILE
 StatCheck $?
 
 Print " Update Roboshop Configuration "
-mv localhost.conf /etc/nginx/default.d/roboshop.conf  >> $LOG_FILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>> $LOG_FILE
 StatCheck $?
 
 Print " Start Nginx "
-systemctl enable nginx >> $LOG_FILE && systemctl restart nginx  >> $LOG_FILE
+systemctl enable nginx >> $LOG_FILE && systemctl restart nginx  &>> $LOG_FILE
 StatCheck $?
