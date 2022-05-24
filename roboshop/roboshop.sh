a666c8fafec0461420f64beee3ff0647f1db0b26
Purushotham Pt <purushothampt@gmail.com>
2022-05-23 21:33:52 -0500

    Make file changes - FIX 3

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ ! -e components/$1.sh ]; then
-  echo "/e[38m Component file does not exist/e[0m"
+  echo -e "/e[38m Component file does not exist/e[0m"
   exit
 fi
 bash components/$1.sh
