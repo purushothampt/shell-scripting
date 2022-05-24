29bfc0f21455c9a71ea1df9551d35a6776f19f57
Purushotham Pt <purushothampt@gmail.com>
2022-05-23 21:57:20 -0500

    Make file changes - UID

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,10 +9,20 @@ fi
 
 echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
-systemctl start nginx
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[33m SUCCESS \e[0m"
+else
+  echo -e "\e31m FAILURE \e[0m"
+  exit 1
+fi
 echo -e "\e[36m Downloading Nginx Content \e[0m "
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $? -eq 0 ]; then
+  echo -e "\e[33m SUCCESS \e[0m"
+else
+  echo -e "\e31m FAILURE \e[0m"
+  exit 1
+fi
 
 echo -e "\e[36m Cleanup old content and extract new archive \e[0m"
 rm -rf /usr/share/nginx/html/*
@@ -23,6 +33,18 @@ mv static/* .
 #rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+if [ $? -eq 0 ]; then
+  echo -e "\e[33m SUCCESS \e[0m"
+else
+  echo -e "\e31m FAILURE \e[0m"
+  exit 1
+fi
 echo -e "\e[36m Start Nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
+if [ $? -eq 0 ]; then
+  echo -e "\e[33m SUCCESS \e[0m"
+else
+  echo -e "\e31m FAILURE \e[0m"
+  exit 1
+fi
