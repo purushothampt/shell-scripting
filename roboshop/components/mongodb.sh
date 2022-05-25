15dfba4a851b5620a6fe36804a2bb093df657216
Purushotham Pt <purushothampt@gmail.com>
2022-05-25 16:56:26 -0500

    Mongo DB Load Schema

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -23,7 +23,7 @@ curl -f -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mo
 StatCheck $?
 
 Print "Extract Schema"
-cd /tmp &>> $LOG_FILE && unzip mongodb.zip  &>> $LOG_FILE
+cd /tmp &>> $LOG_FILE && unzip -o mongodb.zip  &>> $LOG_FILE
 StatCheck $?
 
 Print "Load Schema"
