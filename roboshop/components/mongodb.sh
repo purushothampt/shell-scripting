1a5b1c45b2baffdb91659e4e91535a306c80a9f8
Purushotham Pt <purushothampt@gmail.com>
2022-05-24 22:08:10 -0500

    sed

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -25,18 +25,18 @@ if [ $USER_ID -ne 0 ]; then
 fi
 
 
-print "Setup MongoDB repos"
+Print "Setup MongoDB repos"
 curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> $LOG_FILE
 StatCheck $?
 
-print "Install MongoDB"
+Print "Install MongoDB"
 yum install -y mongodb-org &>> $LOG_FILE
 StatCheck $?
 
-print "Update Listen IP address"
+Print "Update Listen IP address"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOG_FILE
 StatCheck $?
 
-print "Start MongoDB"
+Print "Start MongoDB"
 systemctl enable mongod &>> $LOG_FILE && systemctl restart mongod &>> $LOG_FILE
 StatCheck $?
