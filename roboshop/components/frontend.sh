005df16af3e12c6a4509b4b1aaf1bcfdb7ebcd77
Purushotham Pt <purushothampt@gmail.com>
2022-05-24 22:19:36 -0500

    common.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,28 +1,6 @@
 #! /bin/bash
 
-USER_ID=$(id -u)
-
-StatCheck() {
-if [ $1 -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
-}
-
-Print() {
-  echo -e "\n ----------------- $1 --------------------" >> $LOG_FILE
-  echo -e "\e[36m $1 \e[0m"
-}
-
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
-
-if [ $USER_ID -ne 0 ]; then
-  echo -e "\e[31m User should be a Root User \e[0m"
-  exit 1
-fi
+source components/common.sh
 
 Print " Installing Nginx "
 yum install nginx -y &>> $LOG_FILE
