268c5afc3bebe0cfe3f0d11336fbbea83c022f06
Purushotham Pt <purushothampt@gmail.com>
2022-05-26 14:10:39 -0500

    Catalogue component - Fix 1

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -10,30 +10,30 @@ source components/common.sh
 #2. Now, lets set up the service with systemctl.
 
 Print "Configure Yum repos"
-curl -f -sL https://rpm.nodesource.com/setup_lts.x | bash &>> LOG_FILE
+curl -f -sL https://rpm.nodesource.com/setup_lts.x | bash - &>> $LOG_FILE
 StatCheck $?
 
 Print "Install NodeJS"
-yum install nodejs gcc-c++ -y &>> LOG_FILE
+yum install nodejs gcc-c++ -y &>> $LOG_FILE
 StatCheck $?
 
 Print "Download App Dependencies"
-curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>> LOG_FILE
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>> $LOG_FILE
 StatCheck $?
 
 Print "Clear old Content"
-rm -rf /home/roboshop &>> LOG_FILE
+rm -rf /home/roboshop &>> $LOG_FILE
 StatCheck $?
 
 Print "Extract App Dependencies"
-cd /home/roboshop &>> LOG_FILE && unzip /tmp/catalogue.zip &>> LOG_FILE && mv catalogue-main catalogue &>> LOG_FILE
+cd /home/roboshop &>> $LOG_FILE && unzip /tmp/catalogue.zip &>> $LOG_FILE && mv catalogue-main catalogue &>> $LOG_FILE
 StatCheck $?
 
 Print "Install App Dependencies"
-cd /home/roboshop/catalogue &>> LOG_FILE && npm install &>> LOG_FILE
+cd /home/roboshop/catalogue &>> $LOG_FILE && npm install &>> $LOG_FILE
 StatCheck $?
 
 #Print "App User Permission"
-#chown - R roboshop:roboshop /homo/roboshop &>> LOG_FILE
+#chown - R roboshop:roboshop /homo/roboshop &>> $LOG_FILE
 #StatCheck $?
 
