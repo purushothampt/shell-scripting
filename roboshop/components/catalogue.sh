a12a529929396e12e064ccc432d36af838eb8f45
Purushotham Pt <purushothampt@gmail.com>
2022-05-26 14:42:23 -0500

    Catalogue component - Fix 3

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -17,27 +17,28 @@ Print "Install NodeJS"
 yum install nodejs gcc-c++ -y &>> $LOG_FILE
 StatCheck $?
 
-Print "Add User"
 id $APP_USER &>> $LOG_FILE
 if [ $? -ne 0 ];then
+  Print "Add User"
   useradd roboshop &>> $LOG_FILE
+  StatCheck $?
 fi
-StatCheck $?
+
 
 Print "Download App Dependencies"
 curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>> $LOG_FILE
 StatCheck $?
 
 Print "Clear old Content"
-rm -rf /home/roboshop/catalogue &>> $LOG_FILE
+rm -rf /home/$APP_USER/catalogue &>> $LOG_FILE
 StatCheck $?
 
 Print "Extract App Dependencies"
-cd /home/roboshop &>> $LOG_FILE && unzip /tmp/catalogue.zip &>> $LOG_FILE && mv catalogue-main catalogue &>> $LOG_FILE
+cd /home/$APP_USER &>> $LOG_FILE && unzip /tmp/catalogue.zip &>> $LOG_FILE && mv catalogue-main catalogue &>> $LOG_FILE
 StatCheck $?
 
 Print "Install App Dependencies"
-cd /home/roboshop/catalogue &>> $LOG_FILE && npm install &>> $LOG_FILE
+cd /home/$APP_USER/catalogue &>> $LOG_FILE && npm install &>> $LOG_FILE
 StatCheck $?
 
 #Print "App User Permission"
