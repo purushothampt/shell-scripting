6062350c938434e5da8ff87f64df81c8ba18b046
Purushotham Pt <purushothampt@gmail.com>
2022-05-27 18:41:12 -0500

    Catalogue component - Fix 6

---
+++ roboshop/components/mysql.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+Source components/common.sh
+
+# curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+# yum install mysql-community-server -y
+# systemctl enable mysqld
+# systemctl start mysqld
+
+Print "Setup MySQL Repos"
+curl -f -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>> $LOG_FILE
+StatCheck $?
+
+Print "Install MySQl"
+yum install mysql-community-server -y &>> $LOG_FILE
+StatCheck $?
+
+Print "Start MySQl service"
+systemctl enable mysqld &>> $LOG_FILE && systemctl start mysqld &>> $LOG_FILE
+StatCheck $?
+
+
+
