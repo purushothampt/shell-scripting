d5abae48f097e321ac73fc9ba29abce1f6b0cec6
Purushotham Pt <purushothampt@gmail.com>
2022-05-27 23:23:44 -0500

    Catalogue component - Fix 6

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -19,8 +19,13 @@ Print "Start MySQl service"
 systemctl enable mysqld &>> $LOG_FILE && systemctl start mysqld &>> $LOG_FILE
 StatCheck $?
 
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
-DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>> $LOG_FILE
+if [ $? -ne 0 ]; then
+  Print "Change Default root Password"
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
+  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>> $LOG_FILE
+  StatCheck$?
+fi
 
 
