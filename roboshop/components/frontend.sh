bd01729e03700bb5b707e564eb65a9cd28535470
Purushotham Pt <purushothampt@gmail.com>
2022-05-29 13:05:00 -0500

    Shipping

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -27,6 +27,7 @@ StatCheck $?
 Print " Update Roboshop Config file"
 for component in catalogue user cart shipping; do
   Print "Updating $component in configuration"
+  echo $component
   sed -i -e '/$component/s/localhost/$component.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>> $LOG_FILE
   StatCheck $?
 done
