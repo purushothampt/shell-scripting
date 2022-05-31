e782fd148c1bfb3d2fcb23d4bd560752575baf30
Purushotham Pt <purushothampt@gmail.com>
2022-05-31 12:40:50 -0500

    sed FIX

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -50,15 +50,15 @@ SERVICE_SETUP() {
  chown -R $APP_USER:$APP_USER /home/$APP_USER &>> $LOG_FILE
  StatCheck $?
 
- Print "Setup systemD file"
- sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
-        -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
-        -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
-        -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
-        -e 's/CARTENDPOINT/shipping.roboshop.internal/' \
-        -e 's/DBHOST/mysql.roboshop.internal/' \
-        /home/roboshop/$COMPONENT/systemd.service &>> $LOG_FILE && mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service &>> $LOG_FILE
- StatCheck $?
+# Print "Setup systemD file"
+# sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
+#        -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
+#        -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
+#        -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
+#        -e 's/CARTENDPOINT/shipping.roboshop.internal/' \
+#        -e 's/DBHOST/mysql.roboshop.internal/' \
+#        /home/roboshop/$COMPONENT/systemd.service &>> $LOG_FILE && mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service &>> $LOG_FILE
+# StatCheck $?
 
  Print "Restart $COMPONENT service"
  systemctl daemon-reload &>> $LOG_FILE && systemctl start $COMPONENT &>> $LOG_FILE && systemctl enable $COMPONENT &>> $LOG_FILE
