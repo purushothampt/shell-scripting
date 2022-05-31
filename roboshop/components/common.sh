9879138e9ab507326a317754155d8057683e068e
Purushotham Pt <purushothampt@gmail.com>
2022-05-31 15:07:34 -0500

    sed FIX

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -51,14 +51,15 @@ SERVICE_SETUP() {
  StatCheck $?
 
  Print "Setup systemD file"
- sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
+ sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
+  /home/roboshop/$COMPONENT/systemd.service &>> $LOG_FILE && mv /home/roboshop/$COMPONENT/systemd.service
+  etc/systemd/system/$COMPONENT.service &>> $LOG_FILE
 #        -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
 #        -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
 #        -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
 #        -e 's/CARTENDPOINT/shipping.roboshop.internal/' \
 #        -e 's/DBHOST/mysql.roboshop.internal/' \
-        /home/roboshop/$COMPONENT/systemd.service &>> $LOG_FILE && mv /home/roboshop/$COMPONENT/systemd.service
- etc/systemd/system/$COMPONENT.service &>> $LOG_FILE
+
  StatCheck $?
 
  Print "Restart $COMPONENT service"
