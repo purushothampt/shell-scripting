bcb4fb4f21511f1c6bac988dee1f43c3400d9462
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 16:19:42 -0500

    Practice : frontend Initial

--- practice1/components/frontend.sh
+++ practice1/components/frontend.sh
@@ -11,24 +11,24 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 echo -e "\e[33m Installing NGINX \e[0m"
-yum install nginx -y
+yum install nginx -y &>> $LOG_FILE
 
 echo -e "\e[33m Start NGINX \e[0m"
-systemctl enable nginx && systemctl start nginx
+systemctl enable nginx && systemctl start nginx &>> $LOG_FILE
 
 echo -e "\e[33m Download yum repos \e[0m"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $LOG_FILE
 
 echo -e "\e[33m Caleaning old Files \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
 
 cd /usr/share/nginx/html
 
 echo -e "\e[33m Extract from Archives \e[0m"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && mv localhost.conf /etc/nginx/default.d/roboshop.conf
+unzip /tmp/frontend.zip &>> $LOG_FILE && mv frontend-main/* . &>> $LOG_FILE && mv static/* . &>> $LOG_FILE && mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG_FILE
 
 echo -e "\e[33m Restart NGINX \e[0m"
-systemctl restart nginx
+systemctl restart nginx &>> $LOG_FILE
 
 
 
