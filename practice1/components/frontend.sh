38bd7e7201fe08471c132ddbf71934a78d8493f8
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 13:57:52 -0500

    Practice : frontend Initial

--- practice1/components/frontend.sh
+++ practice1/components/frontend.sh
@@ -2,33 +2,33 @@
 
 source components/common.sh
 
-echo -e "\e[36m Install nginx \e[0m"
+Print " Install nginx "
 yum install nginx -y &>> $LOG_FILE
 StatCheck $?
 
 
-echo -e "\e[36m Start and Enable nginx \e[0m"
+Print " Start and Enable nginx "
 systemctl enable nginx && systemctl start nginx &>> $LOG_FILE
 StatCheck $?
 
-echo -e "\e[36m Download yum repos \e[0m"
+Print " Download yum repos "
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $LOG_FILE
 StatCheck $?
 
-echo -e "\e[36m clear old contents \e[0m"
+Print " clear old contents "
 rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
 StatCheck $?
 
 cd /usr/share/nginx/html/
 
-echo -e "\e[36m Extracting Archives \e[0m"
+Print " Extracting Archives "
 unzip /tmp/frontend.zip &>> $LOG_FILE && mv frontend-main/* . &>> $LOG_FILE && mv static/* . &>> $LOG_FILE
 StatCheck $?
 
-echo -e "\e[36m update roboshop configuration \e[0m"
+Print " update roboshop configuration "
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG_FILE
 StatCheck $?
 
-echo -e "\e[36m restart nginx \e[0m"
+Print " restart nginx "
 systemctl restart nginx &>> $LOG_FILE
 StatCheck $?
