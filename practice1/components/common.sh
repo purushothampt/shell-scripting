c8cb368490ff0096960c65f3f4550e3056de8070
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 13:33:58 -0500

    Practice : frontend Initial

--- practice1/components/common.sh
+++ practice1/components/common.sh
@@ -2,11 +2,11 @@
 
 USER_ID=$(id -u)
 
-if USER_ID [ -ne 0 ]; then
+if $USER_ID [ -ne 0 ]; then
   echo -e "\e[31m user must be a root user \e[0m"
   exit 1
 fi
 
 LOG_FILE = /tmp/roboshop.log
-rm -rf LOG_FILE
+rm -rf $LOG_FILE
 
