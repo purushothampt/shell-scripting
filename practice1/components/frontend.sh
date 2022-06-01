5512dd15d109f828d668000c4756d9c282017c27
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 16:17:39 -0500

    Practice : frontend Initial

--- practice1/components/frontend.sh
+++ practice1/components/frontend.sh
@@ -7,20 +7,27 @@ if [ $USER_ID -ne 0 ];then
   exit 1
 fi
 
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+
+echo -e "\e[33m Installing NGINX \e[0m"
 yum install nginx -y
 
+echo -e "\e[33m Start NGINX \e[0m"
 systemctl enable nginx && systemctl start nginx
 
+echo -e "\e[33m Download yum repos \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+echo -e "\e[33m Caleaning old Files \e[0m"
 rm -rf /usr/share/nginx/html/*
 
 cd /usr/share/nginx/html
 
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+echo -e "\e[33m Extract from Archives \e[0m"
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
+echo -e "\e[33m Restart NGINX \e[0m"
 systemctl restart nginx
 
 
