e03adcaacaaa48158d102ec941cdeddd3aa0e16b
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 16:09:02 -0500

    Practice : frontend Initial

---
+++ practice1/components/frontend.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+yum install nginx -y
+
+systemctl enable nginx && systemctl start nginx
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+rm -rf /usr/share/nginx/html/*
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
+
+
+
