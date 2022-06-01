17537f0727d5187743d25e9b972c90c34ee60b4a
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 16:36:58 -0500

    Practice : frontend Initial

--- practice1/components/frontend.sh
+++ practice1/components/frontend.sh
@@ -1,70 +1,30 @@
 #! /bin/bash
 
-USER_ID=$(id -u)
-
-if [ $USER_ID -ne 0 ];then
-  echo -e "\e[31m User should be a root user \e[0m"
-  exit 1
-fi
-
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
-
-echo -e "\e[33m Installing NGINX \e[0m"
+Print " Install NGINX "
 yum install nginx -y &>> $LOG_FILE
-if [ $? -eq 0 ];then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $?
 
-echo -e "\e[33m Start NGINX \e[0m"
+Print " Start NGINX "
 systemctl enable nginx && systemctl start nginx &>> $LOG_FILE
-if [ $? -eq 0 ];then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $?
 
-echo -e "\e[33m Download yum repos \e[0m"
+Print " Download yum repos "
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $LOG_FILE
-if [ $? -eq 0 ];then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $?
 
-echo -e "\e[33m Caleaning old Files \e[0m"
+Print " Caleaning old Files "
 rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
-if [ $? -eq 0 ];then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $?
 
 cd /usr/share/nginx/html
 
-echo -e "\e[33m Extract from Archives \e[0m"
+Print " Extract from Archives "
 unzip /tmp/frontend.zip &>> $LOG_FILE && mv frontend-main/* . &>> $LOG_FILE && mv static/* . &>> $LOG_FILE && mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG_FILE
-if [ $? -eq 0 ];then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $?
 
-echo -e "\e[33m Restart NGINX \e[0m"
+Print " Restart NGINX "
 systemctl restart nginx &>> $LOG_FILE
-if [ $? -eq 0 ];then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $?
 
 
 
