90dc440c7e1b6b93ee53cdeeaa65b8813c3c02b6
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 13:51:11 -0500

    Practice : frontend Initial

--- practice1/components/common.sh
+++ practice1/components/common.sh
@@ -10,3 +10,11 @@ fi
 LOG_FILE = /tmp/roboshop.log
 rm -f $LOG_FILE
 
+StatCheck(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+    exit 2
+  fi
+}
--- practice1/components/frontend.sh
+++ practice1/components/frontend.sh
@@ -3,25 +3,32 @@
 source components/common.sh
 
 echo -e "\e[36m Install nginx \e[0m"
-yum install nginx -y
+yum install nginx -y &>> $LOG_FILE
+StatCheck $?
+
 
 echo -e "\e[36m Start and Enable nginx \e[0m"
-systemctl enable nginx && systemctl start nginx
+systemctl enable nginx && systemctl start nginx &>> $LOG_FILE
+StatCheck $?
 
 echo -e "\e[36m Download yum repos \e[0m"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $LOG_FILE
+StatCheck $?
 
 echo -e "\e[36m clear old contents \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
+StatCheck $?
 
 cd /usr/share/nginx/html/
 
 echo -e "\e[36m Extracting Archives \e[0m"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . &>> $LOG_FILE
+StatCheck $?
 
 echo -e "\e[36m update roboshop configuration \e[0m"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG_FILE
+StatCheck $?
 
 echo -e "\e[36m restart nginx \e[0m"
-systemctl restart nginx
-
+systemctl restart nginx &>> $LOG_FILE
+StatCheck $?
