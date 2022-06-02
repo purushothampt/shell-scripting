4bdd73ec88bed68b69960590a22a8e74009c9dc0
Purushotham Pt <purushothampt@gmail.com>
2022-06-01 20:24:35 -0500

    Practice : frontend Initial

--- practice1/components/redis.sh
+++ practice1/components/redis.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+source components/common.sh
+
 Print " Download Redis Reops "
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> $LOG_FILE
 StatCheck $?
