76ed649694bd7d7d3e17f169d1248c7430c910f8
Purushotham Pt <purushothampt@gmail.com>
2022-06-02 13:10:11 -0500

    Practice : frontend Initial

--- practice1/components/rabbitmq.sh
+++ practice1/components/rabbitmq.sh
@@ -2,16 +2,12 @@
 
 source components/common.sh
 
-Print " Install Erlang dependency for RabbitMQ "
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOG_FILE
-StatCheck $?
-
 Print " Setup Yum Repos for Rabbit MQ "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOG_FILE
 StatCheck $?
 
-Print " Install Rabbit MQ "
-yum install rabbitmq-server -y &>> $LOG_FILE
+Print " Install Erlang dependency for RabbitMQ "
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>> $LOG_FILE
 StatCheck $?
 
 Print " Start RabbitMQ "
