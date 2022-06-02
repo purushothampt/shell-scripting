c892243f5a0dc324c6195121a837781d8778713e
Purushotham Pt <purushothampt@gmail.com>
2022-06-02 11:42:22 -0500

    Practice : frontend Initial

--- practice1/components/mysql.sh
+++ practice1/components/mysql.sh
@@ -17,8 +17,14 @@ StatCheck $?
 # Need to grep for old password in log and move to a variable
 # Below SET command is used to update the Password, we are storing the SQL query i a file
 # connect to my SQL with Default Password and then UPDATE new password
-Print " Change Default Password "
-DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpass.sql
-mysql --connect-expired-password -uroot -p$DEFAULT_PASSWORD < /tmp/rootpass.sql &>> $LOG_FILE
-StatCheck $?
+
+# we need to check if password is already signed, so we need to login wto mysql ith new password, if it works then password
+# is changed
+echo 'show databases' | mysql -uroot -uRoboShop@1 &>> $LOG_FILE
+if [ $? -ne 0 ]; then
+  Print " Change Default Password "
+  DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpass.sql
+  mysql --connect-expired-password -uroot -p$DEFAULT_PASSWORD < /tmp/rootpass.sql &>> $LOG_FILE
+  StatCheck $?
+fi
