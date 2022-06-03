69795398776c5776d005bbde521e203dd7074905
Purushotham Pt <purushothampt@gmail.com>
2022-06-03 18:15:54 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -15,6 +15,6 @@ aws ec2 run-instances \
   --instance-type t2.micro \
   --security-group-ids $SG_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
   | jq
 
