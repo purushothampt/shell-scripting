a5ff4e5071bdf9d0f3d0250bb080c1c5569a9b8f
Purushotham Pt <purushothampt@gmail.com>
2022-06-03 18:17:54 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -9,12 +9,12 @@ COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow_all_traffic | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
-aws ec2 run-instances \
+PRIVATE_IP=$(aws ec2 run-instances \
   --image-id $AMI_ID \
   --count 1 \
   --instance-type t2.micro \
   --security-group-ids $SG_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-  | jq
-
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" '.Instances[].PrivateIpAddress[]'
+  | jq)
+echo $PRIVATE_IP
