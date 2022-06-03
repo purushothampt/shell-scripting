f42eb64e2d5fafc619976f29bd007e0c4e72b2b7
Purushotham Pt <purushothampt@gmail.com>
2022-06-03 16:09:59 -0500

    Practice : frontend Initial

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,6 +6,7 @@ if [ -z $1 ];then
 fi
 
 COMPONENT=$1
+ZONE_ID="Z07728992EJSXZZKABHJD"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow_all_traffic | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
@@ -18,4 +19,6 @@ PRIVATE_IP=$(aws ec2 run-instances \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent ,InstanceInterruptionBehavior=stop}" \
   | jq '."Instances"[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo $PRIVATE_IP
+sed -e "s/COMPONENT/$COMPONENT/" -e "s/IPADDRESS/$PRIVATE_IP" route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json | jq
+
