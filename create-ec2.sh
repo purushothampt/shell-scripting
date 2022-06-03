0c18326bd4296d177e4bd523218647c64ced8586
Purushotham Pt <purushothampt@gmail.com>
2022-06-03 16:48:48 -0500

    Practice : frontend Initial

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,18 +11,18 @@ ZONE_ID="Z07728992EJSXZZKABHJD"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow_all_traffic | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
-PRIVATE_IP=$(aws ec2 run-instances \
-  --image-id $AMI_ID \
-  --instance-type t2.micro \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
-  --security-group-ids $SGID \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent ,InstanceInterruptionBehavior=stop}" \
-  | jq '."Instances"[].PrivateIpAddress' | sed -e 's/"//g')
 
+for component in cart catalogue user shipping payment frontend rabbitmq redis mysql mongodb ; do
+  PRIVATE_IP=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t2.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
+    --security-group-ids $SGID \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent ,InstanceInterruptionBehavior=stop}" \
+    | jq '."Instances"[].PrivateIpAddress' | sed -e 's/"//g')
+  sed -e "s/COMPONENT/$COMPONENT/" -e "s/PRIVATE_IP/$PRIVATE_IP/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json | jq
+done
 
-ZN_ID=$(aws route53 list-hosted-zones-by-name --dns-name roboshop.internal) | jq '.HostedZones[].DNSName'
-echo $ZN_ID
-sed -e "s/COMPONENT/$COMPONENT/" -e "s/PRIVATE_IP/$PRIVATE_IP/" route53.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json | jq
 
 
