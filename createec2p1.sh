0115e07e4cf86e6673c39367cda15a375ae5e995
Purushotham Pt <purushothampt@gmail.com>
2022-06-03 18:01:27 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+if [ -z $1 ]; then
+  echo -e 'e\[31m Machine name is needed \e[0m'
+fi
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow_all_traffic | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
-echo $SG_ID
-#aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --security-group-ids $SG_ID
+
+aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --security-group-ids $SG_ID
