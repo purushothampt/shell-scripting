d15346eb15c348987c56ccba4090ba971ea9d885
Purushotham Pt <purushothampt@gmail.com>
2022-06-02 22:36:54 -0500

    Practice : frontend Initial

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,5 +11,11 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow_all_traffic | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 echo $AMI_ID
 echo $SGID
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" --security-group-ids $SGID | jq
+aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --instance-type t2.micro \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
+  --security-group-ids $SGID \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent ,InstanceInterruptionBehavior=stop}" \
+  | jq
 
