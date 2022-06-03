f589610560424a17e96398e5346ecc85c80eb3d5
Purushotham Pt <purushothampt@gmail.com>
2022-06-02 21:55:23 -0500

    Practice : frontend Initial

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,4 +2,4 @@
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
-echo -e "Hello"
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
