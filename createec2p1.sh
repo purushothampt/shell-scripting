e38afbafb5f89dac588fad8a3a4322d9ccb76a76
Purushotham Pt <purushothampt@gmail.com>
2022-06-03 17:45:13 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-AMI_ID=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=Centos-7-DevOps-Practice")
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice")
 echo $AMI_ID
