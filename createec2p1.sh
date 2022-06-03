43bc186104c2fb2f38c1ae464aa88fce378b6fac
Purushotham Pt <purushothampt@gmail.com>
2022-06-03 18:14:59 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -5,7 +5,16 @@ if [ -z $1 ]; then
   exit 1
 fi
 
+COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow_all_traffic | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
-aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --security-group-ids $SG_ID
+aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --count 1 \
+  --instance-type t2.micro \
+  --security-group-ids $SG_ID \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
+  | jq
+
