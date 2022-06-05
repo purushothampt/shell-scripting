549070eb68667cef9a45ef4d54e82119eea5758e
Purushotham Pt <purushothampt@gmail.com>
2022-06-04 21:21:12 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -1,2 +1,2 @@
-PRIVATE_IP=$(aws ec2 describe-instances  --filters "Name=tag-value,Values=workstation" | jq)
+aws ec2 describe-instances  --filters "Name=tag-value,Values=workstation" | jq
 #echo $PRIVATE_IP
