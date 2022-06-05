6e01aca1d8d49114e2933ae97e029e3bf37b288a
Purushotham Pt <purushothampt@gmail.com>
2022-06-04 22:18:19 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -2,6 +2,7 @@
 
 PRIVATE_IP=$(aws ec2 describe-instances  --filters "Name=tag-value,Values=workstation" --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text)
 AMI_ID=$(aws ec2 describe-images --filters "Name='name',Values=Centos-7-DevOps-Practice" --query "Images[*].[ImageId]" --output text)
-aws ec2 describe-security-groups --filters "Name=group-name,Values=Allow_all_traffic" --query "SecurityGroups[*].[GroupId]" --output text
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=Allow_all_traffic" --query "SecurityGroups[*].[GroupId]" --output text)
+aws route53 list-hosted-zones --output yaml
 
 
