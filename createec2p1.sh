d83ecffc2b9c3fc15a7cd6340fa39fd9984e4394
Purushotham Pt <purushothampt@gmail.com>
2022-06-04 21:52:34 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -1,2 +1,2 @@
-PRIVATE_IP=$(aws ec2 describe-instances  --filters "Name=tag-value,Values=workstation" | jq '.Instances[].PrivateIpAddress')
-echo $PRIVATE_IP
+aws ec2 describe-instances  --filters "Name=tag-value,Values=workstation" --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text
+
