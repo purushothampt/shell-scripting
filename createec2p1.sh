5b0c207a32493ac0d3ff6e4264e1d483540dca16
Purushotham Pt <purushothampt@gmail.com>
2022-06-04 22:06:44 -0500

    Practice : frontend Initial

--- createec2p1.sh
+++ createec2p1.sh
@@ -1,7 +1,7 @@
 
 
 PRIVATE_IP=$(aws ec2 describe-instances  --filters "Name=tag-value,Values=workstation" --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text)
-aws ec2 describe-images --filters "Name='name',Values=Centos-7-DevOps-Practice" --query "Images[*].[ImageId]" --output text
+AMI_ID=$(aws ec2 describe-images --filters "Name='name',Values=Centos-7-DevOps-Practice" --query "Images[*].[ImageId]" --output text)
 
 
 
